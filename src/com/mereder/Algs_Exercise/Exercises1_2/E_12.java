3363c0a985a9df6e2c2665e3ee05ab7d9df921c8
Mereder <550199493@qq.com>
2018-07-26 11:13:04 +0800

    Continualy updating
    11 、12（Zeller Formula）

--- src/com/mereder/Algs_Exercise/Exercises1_2/E_12.java
+++ src/com/mereder/Algs_Exercise/Exercises1_2/E_12.java
@@ -1,12 +1,10 @@
 package com.mereder.Algs_Exercise.Exercises1_2;
 
-import edu.princeton.cs.algs4.Date;
-
 /**
- * 根据Date的API 实现一个SmartDate类型
- * 在日期非法时抛出一个异常
+ * 添加一个dayOfweek的方法，为日期中每周日返回Monday、Tuesday、Wednesday
+ * Thursday、Friday、Saturday、Sunday 假定时间是21世纪
  */
-public class E_11 {
+public class E_12 {
     public static class SmartDate  {
         private static final int[] DAYS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
@@ -44,6 +42,7 @@ public class E_11 {
             return year;
         }
 
+
         private static boolean isValid(int m, int d, int y) {
             if (m < 1 || m > 12)      return false;
             if (d < 1 || d > DAYS[m]) return false;
@@ -57,18 +56,40 @@ public class E_11 {
             return y % 4 == 0;
         }
 
+        /** Zeller Formula
+         * @return
+         */
+        public String dayOfWeek(){
+
+                int d = this.getDay();
+                int m = this.getMonth();
+                int y = this.getYear();
+
+                if (m < 3)
+                {
+                    m += 12;
+                    y--;
+                }
+                int w = (d + 2 * m + 3 * (m + 1) / 5 + y + y / 4 - y / 100 + y / 400) % 7;
+
+            String[] dayOfWeek = {"Monday",
+                    "Tuesday",
+                    "Wednesday"
+                    ,"Thursday",
+                    "Friday",
+                    "Saturday",
+                    "Sunday"};
+            return dayOfWeek[w];
+
+        }
         public String toString() {
             return month + "/" + day + "/" + year;
         }
     }
+
     public static void main(String[] args) {
-        SmartDate smartDate = new SmartDate(1,30,2018);
-        System.out.println(smartDate);
-        SmartDate errorDate = new SmartDate(2,30,2018);
-        System.out.println(errorDate);
-        /* 标准输出结果：
-            1/30/2018
-           Exception in thread "main" java.lang.IllegalArgumentException: Invalid date
-        * */
+        SmartDate smartDate = new SmartDate(7,26,2018);
+        String s = smartDate.dayOfWeek();
+        System.out.println(s);
     }
 }
