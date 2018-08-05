ce6ff3efd1777f13bb14fce5bff18c03b4232c2c
Mereder <550199493@qq.com>
2018-08-05 23:10:08 +0800

    1.3.43 文件列表

--- src/com/mereder/Algs_Exercise/Exercises1_3/FileList.java
+++ src/com/mereder/Algs_Exercise/Exercises1_3/FileList.java
@@ -1,4 +1,37 @@
 package com.mereder.Algs_Exercise.Exercises1_3;
 
+import java.io.File;
+
 public class FileList {
+    // 这个队列用的很奇怪，过程中队列中都是只有一个元素
+    Queue<File> queue = new Queue<>();
+    
+    public void addQueue(File file,int depth){
+
+        if (file.exists()){
+            queue.Enqueue(file);
+            for (int i = 0; i < depth; i++) {
+                System.out.print("\t");
+            }
+            System.out.println(queue.Dequeue().getName());
+            if (file.isDirectory()){
+                File[] list = file.listFiles();
+                for (File temp:list){
+                    addQueue(temp,depth+1);
+                }
+            }
+            else
+                return;
+        }
+        else return;
+    }
+
+    public static void main(String[] args) {
+       FileList test = new FileList();
+       String rootname = "E:\\filetest";
+       File root = new File(rootname);
+       test.addQueue(root,0);
+       System.out.println();
+    }
+
 }
