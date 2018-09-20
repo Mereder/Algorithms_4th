3eeae9730145a0ba125f2697d5191fccc2f0c066
Mereder <550199493@qq.com>
2018-09-20 21:43:28 +0800

    1.4.6

---
+++ src/com/mereder/Algs_Exercise/Exercises1_4/E_06.java
@@ -0,0 +1,9 @@
+package com.mereder.Algs_Exercise.Exercises1_4;
+
+/**
+ * a N + N/2 + N/4 + N/8 ... = 2N(1-(1/2)^N) ~  2N 线性
+ * b 1 + 2 + 4 + 8....  N = (1 - 2*N) / 1 - 2 = ~ 2N 线性
+ * c 内层N次  外层 2^b < N  则外层为 lgN  总体增长数量级为 N*lgN 线性对数
+ */
+public class E_06 {
+}
