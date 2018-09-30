725eaac5b0d6b7a2382f513397b64f867058087f
Mereder <550199493@qq.com>
2018-09-30 23:58:18 +0800

    1.4.24

---
+++ src/com/mereder/Algs_Exercise/Exercises1_4/E_24.java
@@ -0,0 +1,13 @@
+package com.mereder.Algs_Exercise.Exercises1_4;
+
+/**
+ * 扔鸡蛋
+ */
+public class E_24 {
+    public static void main(String[] args) {
+
+    }
+    public static int DropEgg(){
+        
+    }
+}
